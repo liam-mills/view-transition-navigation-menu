10c94f10442673dee9cd18bd01506dc9b881e076
liam-mills <liammills.web@gmail.com>
2025-10-15 12:37:57 +0100

    fix: set site and base url in astro config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://liam-mills.github.io',
+  base: '/view-transition-navigation-menu',
   vite: {
     plugins: [tailwindcss()]
   }
